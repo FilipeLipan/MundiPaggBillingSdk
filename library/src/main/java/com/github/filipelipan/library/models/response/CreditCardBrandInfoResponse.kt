74088cb18074eec21b1b0659d0c9ad335d6a221a
Schmidt Jonson <fejn@gft.com>
2018-12-06 17:16:06 -0200

    change credit card info response fragment

--- library/src/main/java/com/github/filipelipan/library/models/response/CreditCardBrandInfoResponse.kt
+++ library/src/main/java/com/github/filipelipan/library/models/response/CreditCardBrandInfoResponse.kt
@@ -1,11 +1,21 @@
 package com.github.filipelipan.library.models.response
 
+import com.google.gson.annotations.SerializedName
+
 class CreditCardBrandInfoResponse(
+        @SerializedName("brand")
         val brand :String,
+        @SerializedName("brandName")
         val brandName :String,
+        @SerializedName("gaps")
         val gaps :List<Int>,
+        @SerializedName("lenghts")
         val lenghts :List<Int>,
+        @SerializedName("mask")
         val mask :String,
-        val cvv :String,
+        @SerializedName("cvv")
+        val cvv :Int,
+        @SerializedName("brandImage")
         val brandImage :String,
+        @SerializedName("possibleBrands")
         val possibleBrands :List<String>)
